e9c7c8d7d058db784fe6497673b5ac21e93899b3
Daniel <jc358600@gmail.com>
2024-12-09 10:19:21 -0300

    testes

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello cold world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, Cold World"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
