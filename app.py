086fb92fafe7e5dd6cd84c01d9c27e53632666c3
Daniel <jc358600@gmail.com>
2024-12-09 11:33:31 -0300

    change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!'
 
 if __name__ == "__main__":
     # Pega a variável de ambiente PORT ou usa 8080 por padrão
